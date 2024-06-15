e6895e2ad0ec947c5da302865795e00af370a13c
Ayush <ayushsri1810@gmail.com>
2024-06-15 16:15:49 +0530

    Noice

---
+++ X  Cycle in a linked list.py
@@ -0,0 +1,84 @@
+# 141. Linked List Cycle
+# Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+# Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [3,2,0,-4], pos = 1
+# Output: true
+# Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+# Example 2:
+
+
+# Input: head = [1,2], pos = 0
+# Output: true
+# Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+# Example 3:
+
+
+# Input: head = [1], pos = -1
+# Output: false
+# Explanation: There is no cycle in the linked list.
+ 
+
+# Constraints:
+
+# The number of the nodes in the list is in the range [0, 104].
+# -105 <= Node.val <= 105
+# pos is -1 or a valid index in the linked-list.
+ 
+
+# Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+#NOTE: simple again we will use two pointers fast and slow im which slow will move one steps and fast will be moving with Two steps at once , there will be a point on which The slow ==fast the Point of node we have to return
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def hasCycle(head):
+    if not head or not head.next:
+        return False
+    
+    slow = fast = head
+    
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    
+    return False
+
+# Example usage
+
+# Creating a cycle linked list: 3 -> 2 -> 0 -> -4 -> 2 (cycle)
+node1 = ListNode(3)
+node2 = ListNode(2)
+node3 = ListNode(0)
+node4 = ListNode(-4)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2  # Creates a cycle
+
+print(hasCycle(node1))  # Output should be True
+
+# Creating a non-cycle linked list: 1 -> 2
+node5 = ListNode(1)
+node6 = ListNode(2)
+node5.next = node6
+
+print(hasCycle(node5))  # Output should be False
+
+# Creating a single node linked list: 1 (no cycle)
+node7 = ListNode(1)
+
+print(hasCycle(node7))  # Output should be False
